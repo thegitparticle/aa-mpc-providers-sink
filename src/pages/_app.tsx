0f5c24c222967ae7813dc925a86b815f736986b2
thegitparticle <maddamsettysanjit@gmail.com>
2023-04-19 11:35:19 +0530

    add: styling setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Albert_Sans } from "next/font/google";
+
+const albertSans = Albert_Sans({
+	variable: "--font-albert-sans",
+	display: "swap",
+	subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<style jsx global>{`
+				:root {
+					--font-albert-sans: ${albertSans.style.fontFamily};
+				}
+			`}</style>
+			<main className={`${albertSans.variable} font-sans`}>
+				<Component {...pageProps} />
+			</main>
+		</>
+	);
 }
