45af210fc5bed28b9994575d0c6471f673675424
thegitparticle <maddamsettysanjit@gmail.com>
2023-04-19 12:26:08 +0530

    add: home indexing setup

---
+++ src/info/providersList.ts
@@ -0,0 +1,16 @@
+export const providersList = [
+	{
+		name: "biconomy",
+		url: "https://biconomy.io/",
+		image: "https://2292242862-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2FIi3tpVbpF01vBC8NJtcj%2Ficon%2FHpClzUxWcnBhlPCn1d2B%2FBiconomy%20Orange%20Icon.svg?alt=media&token=a0f302b3-d77a-4c6d-afd3-77a9637951eb",
+		description: "account abstraction, gasless txns",
+		slug: "biconomy",
+	},
+	{
+		name: "web3Auth",
+		url: "https://web3auth.io/",
+		image: "",
+		description: "social login mpc",
+		slug: "web3auth",
+	},
+];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,31 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { providersList } from "@/info/providersList";
+import { useRouter } from "next/router";
 
 export default function Home() {
+	const router = useRouter();
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<h3>trying aa, mpc providers</h3>
+		<main className="flex min-h-screen flex-col items-center p-24 bg-base-300">
+			<p className="font-bold text-3xl">trying aa, mpc providers</p>
+			<div className="grid grid-cols-2 gap-6 my-8">
+				{providersList.map((provider, key) => (
+					<div key={key} className="card w-96 bg-base-100 shadow-xl">
+						<div className="card-body">
+							<h2 className="card-title">{provider.name}</h2>
+							<p>{provider.description}</p>
+							<div className="card-actions justify-end">
+								<button
+									className="btn btn-primary font-bold"
+									onClick={() =>
+										router.push(`/${provider.slug}`)
+									}
+								>
+									try
+								</button>
+							</div>
+						</div>
+					</div>
+				))}
+			</div>
 		</main>
 	);
 }
